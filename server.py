b1cb1cecd2b757672526db8b0139ae6481e06180
Bambang Wicaksono <bambanx@live.com>
2024-10-28 11:15:39 -0400

    Update Task8

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+Emotion Detector Apps
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,14 +8,16 @@ app = Flask("Emotion Detector Apps")
 
 @app.route("/emotionDetector")
 def sent_detector():
-    # Retrieve the emotion to detect from the request arguments
+    """
+    Retrieve the emotion to detect from the request arguments
+    """
     statement = request.args.get('textToAnalyze')
     # Pass the sentence to the emotion_detector function and store the response
     response = emotion_detector(statement)
 
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-        
+
     # Recreate new object with new dominant emotion property
     modified_response = (
         f"For given statement, the system response is "
@@ -25,6 +30,9 @@ def sent_detector():
     return modified_response
 
 def get_dominant_emotion(obj_result):
+    """
+    Determine doninant emotion according to max values
+    """
     dominant_key = ''
     dominant_val = max(obj_result.values())
     for k,v in obj_result.items():
@@ -34,6 +42,9 @@ def get_dominant_emotion(obj_result):
 
 @app.route("/")
 def render_index_page():
+    """
+    render index page
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
